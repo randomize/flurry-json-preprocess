33322f7996aa90c2b8875c5af06f14b6386aad19
Eugene Mihailenco <mihailencoe@gmail.com>
2019-06-16 22:48:47 +0300

    Readnig: Using bytes now

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,27 @@ use std::io::Write;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let f = File::open("161545.json")?;
-    let file = BufReader::new(&f);
+    let mut file = BufReader::new(&f);
     let mut writer = BufWriter::new(io::stdout());
 
     // Header
     writer.write(b"{ \"data\": [\n").unwrap();
 
+    // Allocate buffer, big enough just in case
+    let mut buf = vec![];
+
     // Read first line as is
-    let mut iter = file.lines();
-    let l = iter.next().unwrap().unwrap(); // result -> option
-    writer.write(l.as_bytes()).unwrap();
+    
+    let mut n = file.read_until(b'\n', &mut buf).expect("Reading failed");
+
+    writer.write(&buf).unwrap();
 
     // Subsequent lines prefixed with ','
-    for line in iter {
+    while n != 0
+    {
         writer.write(b",\n").unwrap();
-        let l = line.unwrap();
-        writer.write(l.as_bytes()).unwrap();
+        n = file.read_until(b'\n', &mut buf).expect("Reading failed");
+        writer.write(&buf).unwrap();
     }
 
     // Footer
